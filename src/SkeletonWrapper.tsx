370c03d14a2f7866dfce1713c490b37521478560
RubenSmn <saaman.ruben@gmail.com>
2022-11-22 11:58:40 +0100

    chore: update mapping children for ts

--- src/SkeletonWrapper.tsx
+++ src/SkeletonWrapper.tsx
@@ -16,10 +16,7 @@ export const SkeletonWrapper = ({
   // if the components are wrapped in a Fragment replace children with the children from the Fragment
   if (children.type === React.Fragment) children = children.props.children;
 
-  // no clue what is better, this
-  return children.map((child, idx) => <Skeleton key={idx}>{child}</Skeleton>);
-  // or this
-  // return React.Children.map(children, (child) => {
-  //   return <Skeleton>{child}</Skeleton>;
-  // });
+  return React.Children.map(children, (child) => {
+    return <Skeleton>{child}</Skeleton>;
+  });
 };
