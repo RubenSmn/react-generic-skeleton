515028fc14f9fb89a270b2c12a678fab2470f4a8
RubenSmn <saaman.ruben@gmail.com>
2022-11-25 16:12:19 +0100

    feat(image): add story descriptions

--- stories/ImageSkeleton.stories.tsx
+++ stories/ImageSkeleton.stories.tsx
@@ -8,17 +8,28 @@ import {
 const meta: Meta = {
   title: "ImageSkeleton",
   component: ImageSkeleton,
+  argTypes: {
+    isLoading: {
+      description: "If `true`, the skeleton will show.",
+      type: "boolean",
+    },
+    round: {
+      description: "If `true`, the skeleton will be round.",
+      type: "boolean",
+    },
+    children: {
+      description: "`HTMLImageElement`",
+      type: "function",
+    },
+  },
 };
 
 export default meta;
 
-const Template: Story<ImageSkeletonProps> = ({ children, ...args }) => (
+const Template: Story<ImageSkeletonProps> = ({ ...args }) => (
   <ImageSkeleton {...args}>
     <img
-      height="160"
-      style={{
-        marginTop: 24,
-      }}
+      height={160}
       src={
         "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
       }
