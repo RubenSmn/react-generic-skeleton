b609c89ff63e53e44625871960f65078e6fccb9e
RubenSmn <saaman.ruben@gmail.com>
2022-11-25 12:23:14 +0100

    feat(stories): add more variants

--- stories/TextSkeleton.stories.tsx
+++ stories/TextSkeleton.stories.tsx
@@ -17,5 +17,21 @@ export const Default = Template.bind({});
 
 Default.args = {
   children:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minus itaque, ea nihil nesciunt illum mollitia exercitationem.",
+  isLoading: true,
+};
+
+export const Short = Template.bind({});
+
+Short.args = {
+  children: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  isLoading: true,
+};
+
+export const Long = Template.bind({});
+
+Long.args = {
+  children:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti pariatur minima velit, odio architecto quisquam assumenda ad eveniet fugit eum? Ipsam minus itaque, ea nihil nesciunt illum mollitia exercitationem.",
+  isLoading: true,
 };
