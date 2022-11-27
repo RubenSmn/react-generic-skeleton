e538a9e2be0f987346c47fa7004b52bcee397188
RubenSmn <saaman.ruben@gmail.com>
2022-11-27 20:15:53 +0100

    chore: export SkeletonProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import "./index.css";
 export * from "./skeletons/TextSkeleton";
 export * from "./skeletons/ListSkeleton";
 export * from "./skeletons/ImageSkeleton";
+export * from "./SkeletonProvider";
 export * from "./SkeletonWrapper";
 export * from "./Skeleton";
