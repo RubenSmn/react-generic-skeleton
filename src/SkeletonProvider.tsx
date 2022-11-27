358ecfc38b7de62d090e840277bece84154fb106
RubenSmn <saaman.ruben@gmail.com>
2022-11-27 19:55:45 +0100

    feat: add skeleton provider to handle the config

---
+++ src/SkeletonProvider.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext } from "react";
+
+interface Animation {
+  animationName?: string;
+  animationDuration?: string;
+  animationTimingFunction?: string;
+  animationDelay?: string;
+}
+
+export interface SkeletonConfig {
+  borderRadius?: number;
+  animation?: Animation;
+  background?: string;
+  backgroundColor?: string;
+}
+
+export interface SkeletonProviderProps {
+  children: React.ReactNode;
+  config?: SkeletonConfig;
+}
+
+export const SkeletonContext = createContext<SkeletonConfig>({});
+
+export const SkeletonProvider = ({
+  children,
+  config,
+}: SkeletonProviderProps) => {
+  return (
+    <SkeletonContext.Provider value={config ?? {}}>
+      {children}
+    </SkeletonContext.Provider>
+  );
+};
+
+export const useSkeletonConfig = () => useContext(SkeletonContext);
