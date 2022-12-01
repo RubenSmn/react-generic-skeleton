e7628ba0ed6c490e59fc35c6f5829dffea60ff94
RubenSmn <saaman.ruben@gmail.com>
2022-12-01 20:00:52 +0100

    chore(stories): update showcase image to octocat

--- stories/ImageSkeleton.stories.tsx
+++ stories/ImageSkeleton.stories.tsx
@@ -42,10 +42,9 @@ const Template: Story<ImageSkeletonProps> = ({ ...args }) => {
   return (
     <ImageSkeleton {...args} isLoading={isLoading}>
       <img
+        src="https://github.com/github/media/blob/master/octocats/octocat.png?raw=true"
         height={160}
-        src={
-          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
-        }
+        alt="github-octocat"
       />
     </ImageSkeleton>
   );
